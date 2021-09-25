13faee025857aa8610b558ad341d0196aee5eb7f
LeandroMenoci <menoci.leandro@gmail.com>
2021-09-25 20:18:47 -0300

    Repetição de Arrays e Condicionais com If

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -4,11 +4,24 @@ import React from 'react';
 import Primeiro from './components/Primeiro'
 import ComParametro from './components/ComParametro'
 import ComFilhos from './components/ComFilhos'
-import Card from './components//layout/Card'
+import Card from './components/layout/Card'
+import Repeticao from './components/Repeticao'
+import Condicional from './components/Condicional'
+import CondicionalComIf from './components/CondicionalComIf';
+
 
 export default props => (
   <div className="App">
-    <Card titulo="Componente Com Filhos">
+    <Card titulo="#06 Condicional v1">
+      <Condicional numero={11}></Condicional>
+    </Card>
+    <Card titulo="#05 Condicional v2">
+      <CondicionalComIf numero={10}></CondicionalComIf>
+    </Card>
+    <Card titulo="#04 Repetição">
+      <Repeticao></Repeticao>
+    </Card>
+    <Card titulo="#03 Componente Com Filhos">
       <ComFilhos>
         <ul>
           <li>Ana</li>
@@ -18,12 +31,12 @@ export default props => (
         </ul>
       </ComFilhos>
     </Card>
-    <Card titulo="Componente Com Parametro">
+    <Card titulo="#02 Componente Com Parametro">
       <ComParametro titulo="Esse é o titulo"
         subtitulo="Esse é o subtitulo" />
     </Card>
 
-    <Card titulo="Primeiro Componente">
+    <Card titulo="#01 Primeiro Componente">
       <Primeiro />
     </Card>
   </div>
---
+++ fundamentos-react/src/data/produto.js
@@ -0,0 +1,5 @@
+export default [
+  { id: 1, nome: 'Caneta', preco: 7.59 },
+  { id: 2, nome: 'Lápis', preco: 2.60 },
+  { id: 3, nome: 'Caderno', preco: 21.99 },
+]
