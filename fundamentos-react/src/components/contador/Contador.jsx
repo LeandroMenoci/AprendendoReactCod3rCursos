bad8fd8c0f6ca5a96a4a84b36242a0d3da1f330b
LeandroMenoci <menoci.leandro@gmail.com>
2021-09-26 13:18:14 -0300

    Tornando o Contador em uma Comunicação Direta

--- fundamentos-react/src/components/contador/Contador.jsx
+++ fundamentos-react/src/components/contador/Contador.jsx
@@ -1,6 +1,8 @@
 import './Contador.css'
 import React, { Component } from 'react';
-
+import Display from './Display';
+import PassoForm from './PassoForm';
+import Botoes from './Botoes';
 
 export default class Contador extends Component {
   state = {
@@ -21,23 +23,21 @@ export default class Contador extends Component {
     })
   }
 
+  mudarPasso = (novoPasso) => {
+    this.setState({
+      passo: novoPasso
+    })
+  }
+
   render() {
     return (
       <div className="Contador">
         <h2>Contador</h2>
-        <div>
-          <label for="passoInput">Passo: </label>
-          <input id="passoInput" type="number" style={{ fontSize: '1.4rem', width: '60px' }}
-            value={this.state.passo}
-            onChange={e => this.setState({ passo: +e.target.value })} />
-        </div>
-        <h4>Valor: {this.state.valor}</h4>
-
-        <div>
-          <button onClick={this.inc}>+</button>
-          <button onClick={this.dec}>-</button>
-        </div>
-      </div>
+        <PassoForm passo={this.state.passo}
+          onPassoChange={this.mudarPasso}></PassoForm>
+        <Display valor={this.state.valor}></Display>
+        <Botoes onInc={this.inc} onDec={this.dec}></Botoes>
+      </div >
     )
   }
 }
---
+++ fundamentos-react/src/components/contador/PassoForm.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default (props) => {
+  return (
+    <div>
+      <label for="passoInput">Passo: </label>
+      <input id="passoInput" type="number" style={{ fontSize: '1.4rem', width: '60px' }}
+        value={props.passo}
+        onChange={e => props.onPassoChange(+e.target.value)} />
+    </div>
+  )
+}
